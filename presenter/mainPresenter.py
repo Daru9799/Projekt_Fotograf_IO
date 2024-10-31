77ca7288f565a2508238c926c97457bf6adf67db
Daru9799 <122099529+Daru9799@users.noreply.github.com>
2024-10-31 11:03:07 +0100

    Wyświetlanie obrazka

--- presenter/mainPresenter.py
+++ presenter/mainPresenter.py
@@ -5,18 +5,19 @@ from PyQt5.QtWidgets import QFileDialog
 class Presenter:
     def __init__(self, view):
         self.view = view
+        self.new_project = Project(None)
 
     #Tworzenie nowego projektu (załadowanie folderu ze zdjęciami)
     def create_new_project(self):
         folder_path = QFileDialog.getExistingDirectory(self.view, "Wybierz folder ze zdjęciami")
         if folder_path:
-            new_project = Project(folder_path)
-            print(f'Wybrano: {new_project.folderPath}') #W ramach testów zeby podejrzeć jaką ściezke przesyła
+            self.new_project.folderPath = folder_path
+            print(f'Wybrano: {self.new_project.folderPath}') #W ramach testów zeby podejrzeć jaką ściezke przesyła
 
             # Wczytanie pliku ze zdjęciami do listy
-            self.load_files(new_project.folderPath)
+            self.load_files(self.new_project.folderPath)
             #Przeslanie sciezki do widoku, zeby zaaktualizowac label
-            self.view.update_folder_path(new_project.folderPath)
+            self.view.update_folder_path(self.new_project.folderPath)
         else:
             print("Nie wybrano folderu.")
 
@@ -28,3 +29,13 @@ class Presenter:
         for filename in os.listdir(folder_path):
             if filename.lower().endswith(('.png', '.jpg', '.jpeg')):  # Tutaj mozna dodac potem wiecej rozszerzen
                 self.view.file_list_widget.addItem(filename)
+
+
+    # Po kliknieciu na prawy panel z plikami uzyskujemy nazwe i wywolujemy odpowiednia funkcje
+    def folder_list_on_click(self, item):
+        file_name = item.text()
+        image_path = os.path.join(self.new_project.folderPath, file_name)
+        print(image_path)
+        self.view.display_image(image_path)
+
+
--- view/mainView.py
+++ view/mainView.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow, QAction, QListWidget, QGridLayout, QWidget, QLabel, QFrame, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QAction, QListWidget, QGridLayout, QWidget, QLabel, QFrame, QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+import os
 
 class MainView(QMainWindow):
     def __init__(self, presenter):
@@ -9,45 +11,48 @@ class MainView(QMainWindow):
 
 ###Na razie lista z plikami, potem będzie tutaj dodawane wszystko co znajduje się w oknie
         # Layout główny
-        main_layout = QGridLayout()
+        self.main_layout = QGridLayout()
 
-        #Lewy panel (lista plikow, dodatkowe informacje EXIF)
-        left_panel_layout = QVBoxLayout()
-        main_layout.addLayout(left_panel_layout,0,2)
+        #Panel prawy (lista plikow, dodatkowe informacje EXIF)
+        right_panel_layout = QVBoxLayout()
+        self.main_layout.addLayout(right_panel_layout,0,2)
 
         # Napis lista plików
-        self.label = QLabel("", self)
+        self.label = QLabel("Nie wybrano folderu!", self)
         self.label.setAlignment(Qt.AlignLeft)
-        left_panel_layout.addWidget(self.label)
+        right_panel_layout.addWidget(self.label)
 
         # Lista plików (wymiary na razie na sztywno)
         self.file_list_widget = QListWidget(self)
-        left_panel_layout.addWidget(self.file_list_widget)
+        right_panel_layout.addWidget(self.file_list_widget)
+        self.file_list_widget.itemClicked.connect(lambda item: self.presenter.folder_list_on_click(item))
 
         #Panel srodkowy
         central_panel_layout = QVBoxLayout()
-        main_layout.addLayout(central_panel_layout,0,1)
+        self.main_layout.addLayout(central_panel_layout,0,1)
 
-        green_frame = QFrame()
-        green_frame.setStyleSheet("background-color: green;")
-        main_layout.addWidget(green_frame)
+        #Obrazek w panelu srodkowym
+        self.graphics_view = QGraphicsView(self)
+        self.scene = QGraphicsScene(self)
+        self.graphics_view.setScene(self.scene)
+        central_panel_layout.addWidget(self.graphics_view)
 
-        #Panel prawy
-        right_panel_layout = QVBoxLayout()
-        main_layout.addLayout(right_panel_layout, 0, 0)
+        #Panel lewy
+        left_panel_layout = QVBoxLayout()
+        self.main_layout.addLayout(left_panel_layout, 0, 0)
 
         red_frame = QFrame()
         red_frame.setStyleSheet("background-color: red;")
-        right_panel_layout.addWidget(red_frame)
+        left_panel_layout.addWidget(red_frame)
 
         #Rozciaganie paneli
-        main_layout.setColumnStretch(0, 1)  # Lewy panel
-        main_layout.setColumnStretch(1, 4)  # Panel środkowy
-        main_layout.setColumnStretch(2, 1)  # Prawy panel
+        self.main_layout.setColumnStretch(0, 1)  # Lewy panel
+        self.main_layout.setColumnStretch(1, 3)  # Panel środkowy
+        self.main_layout.setColumnStretch(2, 1)  # Prawy panel
 
         #Podpięcie do widgetu
         widget = QWidget()
-        widget.setLayout(main_layout)
+        widget.setLayout(self.main_layout)
         self.setCentralWidget(widget)
 
 
@@ -69,6 +74,15 @@ class MainView(QMainWindow):
         self.setWindowIcon(QIcon('img\cameraIcon.png'))  #ikonka w lewym górnym
         self.showMaximized()
 
+###Metody
     #metoda do updatowania sobie tekstu z ścieżką folderu po prawej stronie widgetu
     def update_folder_path(self, folderPath):
         self.label.setText(f"Obrazy z {folderPath}:")
+#obrazek
+    def display_image(self, image_path):
+        pixmap = QPixmap(image_path)
+        if pixmap.isNull():
+            print("Nie udało się załadować obrazu.")
+            return
+        self.scene.clear()
+        self.scene.addItem(QGraphicsPixmapItem(pixmap))
