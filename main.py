cf885867011efc865ebb91b66e51053901b7a1e9
Daru9799 <122099529+Daru9799@users.noreply.github.com>
2024-10-31 20:02:08 +0100

    GUI

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
 from view.mainView import MainView
 from presenter.mainPresenter import Presenter
+from qt_material import apply_stylesheet
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    apply_stylesheet(app, theme='dark_amber.xml') #pelna lista wraz z modyfikacjami https://github.com/UN-GCPDS/qt-material
 
     presenter = Presenter(None)  # Inicjalizacja prezentera bez widoku
     MainWindow = QtWidgets.QMainWindow()
